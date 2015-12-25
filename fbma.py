d48ad436fe74b98a1721581baa369d55ef0b26e7
KEvin Wang <kevinwang@kevinwang.us>
2015-12-25 12:57:47 -0800

    fixed a pretty important off-by-one error

--- fbma.py
+++ fbma.py
@@ -30,102 +30,118 @@ if False:
 .
 .
 </div>"""
-import time
 #handrolling my own semi-html parser cause fuck
-msgs = pd.DataFrame(columns = ["time", "sender", "members", "text"])
-soups = [] #remove this on dec 25th
-df_pre_dict = []
-kkk = None
-def do_stuff(chunk):
-	if(chunk[1] == "/"):
-		print("ALERT ALERT")
-		chunk = chunk[7:]
-	s = BeautifulSoup(chunk).find("div", class_ = "thread")
-	members = [x.strip() for x in next(s.strings).split(",")]
-	try:
-		print(members)
-	except:
-		print('-------------')
-	msg_dict = {"members": members}
-	for msg in s.children:
-		try: 
-			if msg.name == "p":
-				msg_dict["text"] = msg.text
-			elif msg.name == "div":
-				halal = msg.div.contents
-				msg_dict["time"] = halal[1].text
-				msg_dict["sender"] = halal[0].text
-		except AttributeError:
-			print("sucks 2 suk")
-		if msg_dict.get("time") and msg_dict.get("sender"):
-			df_pre_dict.append(msg_dict)
-			msg_dict = {"members": members}
-		#print(msg.encode("utf-8"))
-		#time.sleep(1)
-	#raise ValueError
+def make_df():
+	msgs = pd.DataFrame(columns = ["time", "sender", "members", "text"])
+	soups = [] #remove this on dec 25th
+	df_pre_dict = []
+	kkk = None
+	def do_stuff(chunk):
+		if(chunk[1] == "/"):
+			print("ALERT ALERT")
+			chunk = chunk[7:]
+		s = BeautifulSoup(chunk).find("div", class_ = "thread")
+		members = [x.strip() for x in next(s.strings).split(",")]
+		try:
+			print(members)
+		except:
+			print('-------------')
+		msg_dict = {"members": members}
+		for msg in s.children:
+			try: 
+				if msg.name == "p":
+					msg_dict["text"] = msg.text
+				elif msg.name == "div":
+					halal = msg.div.contents
+					msg_dict["time"] = halal[1].text
+					msg_dict["sender"] = halal[0].text
+			except AttributeError:
+				print("sucks 2 suk")
+			if msg_dict.get("text") and msg_dict.get("sender"):
+				df_pre_dict.append(msg_dict)
+				msg_dict = {"members": members}
+			#print(msg.encode("utf-8"))
+			#time.sleep(1)
+		#raise ValueError
 
-	#soups.append(s)
-	#return s
-x = f.read(1)
-in_a_tag = False
-tags = []
-tag = ""
-chunk = ""
-while(x):
+		#soups.append(s)
+		#return s
 	x = f.read(1)
-	if(x == "<"):
-		in_a_tag = True #we in here
-	elif(x == ">"):
-		if(tag == "/h1"):
-			tag = ""
-			in_a_tag = False
-			break
-		else:
-			tag = ""
-			in_a_tag = False
-	else:
-		if(in_a_tag):
-			tag+=x
-
-print("we are now at the start of the threads")
-#x = f.read(5) #eat the first <div> tag that encapsulates all the threads.  not really necessary to have it I think. ok nevermind it is.
-x = f.read(1) 
-while(x):
-	chunk+= x
-	if(x == ">"):
-		if(in_a_tag):
-			in_a_tag = False
-			if(tag[0] == "/"):
-				try:
-					popped = tags.pop()
-				except:
-					break
-					#true = True
-				if popped.split()[0] == tag.split()[0][1:]:
-					if popped == 'div class="thread"':
-						try:
-							print(chunk[:100])
-						except:
-							for i in chunk[:100]:
-								print(ord(i))
-						do_stuff(chunk)
-						chunk = ""
-					#else:
-						#print(popped, tag)
-					tag = ""
-				else:
-					raise ValueError("END TAG IS TOTALLY DIFFERENT THAN MOST RECENT START TAG {}, {}".format(popped, tag.split()[0]))
-			else:
-				tags.append(tag)
+	in_a_tag = False
+	tags = []
+	tag = ""
+	chunk = ""
+	while(x):
+		x = f.read(1)
+		if(x == "<"):
+			in_a_tag = True #we in here
+		elif(x == ">"):
+			if(tag == "/h1"):
 				tag = ""
+				in_a_tag = False
+				break
+			else:
+				tag = ""
+				in_a_tag = False
 		else:
-			raise ValueError("WE FUCKED UP {}".format(tag))
-	elif(in_a_tag):
-		tag += x
-	elif(x == "<"):
-		assert (not in_a_tag), "rip"
-		in_a_tag = True
-	#print(tag)
-	x = f.read(1)
+			if(in_a_tag):
+				tag+=x
 
-msgs = pd.DataFrame(df_pre_dict)
+	print("we are now at the start of the threads")
+	#x = f.read(5) #eat the first <div> tag that encapsulates all the threads.  not really necessary to have it I think. ok nevermind it is.
+	x = f.read(1) 
+	while(x):
+		chunk+= x
+		if(x == ">"):
+			if(in_a_tag):
+				in_a_tag = False
+				if(tag[0] == "/"):
+					try:
+						popped = tags.pop()
+					except:
+						break
+						#true = True
+					if popped.split()[0] == tag.split()[0][1:]:
+						if popped == 'div class="thread"':
+							try:
+								print(chunk[:100])
+							except:
+								for i in chunk[:100]:
+									print(ord(i))
+							do_stuff(chunk)
+							chunk = ""
+						#else:
+							#print(popped, tag)
+						tag = ""
+					else:
+						raise ValueError("END TAG IS TOTALLY DIFFERENT THAN MOST RECENT START TAG {}, {}".format(popped, tag.split()[0]))
+				else:
+					tags.append(tag)
+					tag = ""
+			else:
+				raise ValueError("WE FUCKED UP {}".format(tag))
+		elif(in_a_tag):
+			tag += x
+		elif(x == "<"):
+			assert (not in_a_tag), "rip"
+			in_a_tag = True
+		#print(tag)
+		x = f.read(1)
+
+	msgs = pd.DataFrame(df_pre_dict)
+	if input("save to csv?  type n to not save. ") != 'n':
+		msgs.to_csv("msgs.csv")
+		print("saved to msgs.csv")
+	else:
+		print("not saved")
+
+if __name__ == "__main__":
+	if input("reanalyze messages.htm?  type n to not. ") != 'n':
+		make_df()
+	else:
+		print("k")
+
+msgs = pd.DataFrame.from_csv("msgs.csv", encoding = "ISO-8859-1")
+
+#def make_words():
+#
