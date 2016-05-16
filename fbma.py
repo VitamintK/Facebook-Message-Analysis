d400210b9116f00920debccb099fd6b42b505e0e
KEvin Wang <kevinwang@kevinwang.us>
2016-05-15 18:02:39 -0700

    testerinos

--- fbma.py
+++ fbma.py
@@ -38,6 +38,195 @@ def make_df():
 					msg_dict["text"] = msg.text
 				elif msg.name == "div":
 					halal = msg.div.contents
+					msg_dict["time"] = pd.to_datetime(halal[1].text)
+					msg_dict["sender"] = halal[0].text
+			except AttributeError:
+				print("sucks 2 suk")
+			if msg_dict.get("text") and msg_dict.get("sender"):
+				df_pre_dict.append(msg_dict)
+				msg_dict = {"members": members}
+	x = f.read(1)
+	in_a_tag = False
+	tags = []
+	tag = ""
+	chunk = ""
+	while(x):
+		x = f.read(1)
+		if(x == "<"):
+			in_a_tag = True #we in here
+		elif(x == ">"):
+			if(tag == "/h1"):
+				tag = ""
+				in_a_tag = False
+				break
+			else:
+				tag = ""
+				in_a_tag = False
+		else:
+			if(in_a_tag):
+				tag+=x
+
+	print("we are now at the start of the threads")
+	x = f.read(1) 
+	while(x):
+		chunk+= x
+		if(x == ">"):
+			if(in_a_tag):
+				in_a_tag = False
+				if(tag[0] == "/"):
+					try:
+						popped = tags.pop()
+					except:
+						break
+					if popped.split()[0] == tag.split()[0][1:]:
+						if popped == 'div class="thread"':
+							try:
+								print(chunk[:100])
+							except:
+								for i in chunk[:100]:
+									print(ord(i))
+							do_stuff(chunk)
+							chunk = ""
+						tag = ""
+					else:
+						raise ValueError("END TAG IS TOTALLY DIFFERENT THAN MOST RECENT START TAG {}, {}".format(popped, tag.split()[0]))
+				else:
+					tags.append(tag)
+					tag = ""
+			else:
+				raise ValueError("WE FUCKED UP {}".format(tag))
+		elif(in_a_tag):
+			tag += x
+		elif(x == "<"):
+			assert (not in_a_tag), "rip"
+			in_a_tag = True
+		x = f.read(1)
+
+	msgs = pd.DataFrame(df_pre_dict)
+	if(input("save to hdf?  type n to not save.") != 'n'):
+		msgs.to_hdf("msgs.h5", 'table', append=False)
+		print("saved to msgs.hd5")
+	else:
+		print("not saved")
+	if input("save to csv?  type n to not save. ") != 'n':
+		msgs.to_csv("msgs.csv")
+		print("saved to msgs.csv")
+	else:
+		print("not saved")
+	return msgs
+
+def test1():
+	msgs = pd.DataFrame(columns = ["time", "sender", "members", "text"])
+	df_pre_dict = []
+	kkk = None
+	def do_stuff(chunk):
+		if(chunk[1] == "/"):
+			print("ALERT ALERT")
+			chunk = chunk[7:]
+		s = BeautifulSoup(chunk).find("div", class_ = "thread")
+		members = [x.strip() for x in next(s.strings).split(",")]
+		try:
+			print(members)
+		except:
+			print('-------------')
+		msg_dict = {"members": members}
+		for msg in s.children:
+			try: 
+				if msg.name == "p":
+					msg_dict["text"] = msg.text
+				elif msg.name == "div":
+					halal = msg.div.contents
+					msg_dict["time"] = pd.to_datetime(halal[1].text)
+					msg_dict["sender"] = halal[0].text
+			except AttributeError:
+				print("sucks 2 suk")
+			if msg_dict.get("text") and msg_dict.get("sender"):
+				df_pre_dict.append(msg_dict)
+				msg_dict = {"members": members}
+	x = f.read(1)
+	in_a_tag = False
+	tags = []
+	tag = ""
+	chunk = ""
+	while(x):
+		x = f.read(1)
+		if(x == "<"):
+			in_a_tag = True #we in here
+		elif(x == ">"):
+			if(tag == "/h1"):
+				tag = ""
+				in_a_tag = False
+				break
+			else:
+				tag = ""
+				in_a_tag = False
+		else:
+			if(in_a_tag):
+				tag+=x
+
+	print("we are now at the start of the threads")
+	x = f.read(1) 
+	while(x):
+		chunk+= x
+		if(x == ">"):
+			if(in_a_tag):
+				in_a_tag = False
+				if(tag[0] == "/"):
+					try:
+						popped = tags.pop()
+					except:
+						break
+					if popped.split()[0] == tag.split()[0][1:]:
+						if popped == 'div class="thread"':
+							try:
+								print(chunk[:100])
+							except:
+								for i in chunk[:100]:
+									print(ord(i))
+							do_stuff(chunk)
+							chunk = ""
+						tag = ""
+					else:
+						raise ValueError("END TAG IS TOTALLY DIFFERENT THAN MOST RECENT START TAG {}, {}".format(popped, tag.split()[0]))
+				else:
+					tags.append(tag)
+					tag = ""
+			else:
+				raise ValueError("WE FUCKED UP {}".format(tag))
+		elif(in_a_tag):
+			tag += x
+		elif(x == "<"):
+			assert (not in_a_tag), "rip"
+			in_a_tag = True
+		x = f.read(1)
+
+	msgs = pd.DataFrame(df_pre_dict)
+	f.close()
+	return msgs
+
+def test2():
+	f = open("C:\\Users\\kevin\\Downloads\\facebook-vitamintK\\html\\messages.htm", 'r', encoding="UTF8")
+
+	msgs = pd.DataFrame(columns = ["time", "sender", "members", "text"])
+	df_pre_dict = []
+	kkk = None
+	def do_stuff(chunk):
+		if(chunk[1] == "/"):
+			print("ALERT ALERT")
+			chunk = chunk[7:]
+		s = BeautifulSoup(chunk).find("div", class_ = "thread")
+		members = [x.strip() for x in next(s.strings).split(",")]
+		try:
+			print(members)
+		except:
+			print('-------------')
+		msg_dict = {"members": members}
+		for msg in s.children:
+			try: 
+				if msg.name == "p":
+					msg_dict["text"] = msg.text
+				elif msg.name == "div":
+					halal = msg.div.contents
 					msg_dict["time"] = halal[1].text
 					msg_dict["sender"] = halal[0].text
 			except AttributeError:
@@ -103,16 +292,20 @@ def make_df():
 		x = f.read(1)
 
 	msgs = pd.DataFrame(df_pre_dict)
-	if input("save to csv?  type n to not save. ") != 'n':
-		msgs.to_csv("msgs.csv")
-		print("saved to msgs.csv")
-	else:
-		print("not saved")
+
+	msgs['time'] =  pd.to_datetime(msgs['time'])
+
+	return msgs
 
 if __name__ == "__main__":
+	#import timeit
+	#y = timeit.timeit('h = test2()', 'from __main__ import test2', number=1) #727 seconds,	460 seconds 
+	#x = timeit.timeit('g = test1()', 'from __main__ import test1', number=1) #1119 seconds,598 seconds
+	#z = timeit.timeit('j = make_df()', 'from __main__ import make_df', number=1) #around 550 seconds...
 	if input("reanalyze messages.htm?  type n to not. ") != 'n':
 		make_df()
 	else:
 		print("k")
 
-msgs = pd.read_csv("msgs.csv", encoding = "ISO-8859-1", parse_dates = [4], keep_default_na = False)
+msgs = pd.read_hdf("msgs.h5", "table", encoding = "ISO-8859-1", parse_dates = [4], keep_default_na = False)
+#msgs = pd.read_csv("msgs.csv", encoding = "ISO-8859-1", parse_dates = [4], keep_default_na = False)
